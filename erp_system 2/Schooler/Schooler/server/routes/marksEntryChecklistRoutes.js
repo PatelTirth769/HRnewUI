578d604e299144a6008dfe6fee4cc12964e012c7
PatelTirth769 <pt569419@gmail.com>
2026-04-30 18:22:31 +0530

    vcfg

--- erp_system 2/Schooler/Schooler/server/routes/marksEntryChecklistRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const marksEntryChecklistController = require('../controllers/marksEntryChecklistController');
-
-router.get('/', marksEntryChecklistController.getChecklistData);
-router.post('/upload', marksEntryChecklistController.uploadChecklistData);
-router.get('/download', marksEntryChecklistController.downloadChecklistData);
-
-module.exports = router;
--- erp_system 2/server/education/routes/gracingCondonationRule.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const gracingCondonationRuleController = require('../controllers/gracingCondonationRuleController');
-
-router.post('/', gracingCondonationRuleController.createGracingCondonationRule);
-router.get('/', gracingCondonationRuleController.getGracingCondonationRules);
-
-module.exports = router;
